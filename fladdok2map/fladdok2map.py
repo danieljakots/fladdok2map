effc9664cdce87461ac4447ebcbf0ab7e5a8cdbf
Daniel Jakots <vigdis@chown.me>
2016-05-17 17:37:09 +0200

    s/app/application/

--- fladdok2map/fladdok2map.py
+++ fladdok2map/fladdok2map.py
@@ -18,17 +18,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from fladdok2map import addok2map
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/apropos')
+@application.route('/apropos')
 def apropos():
     return render_template('apropos.html')
 
-@app.route('/')
+@application.route('/')
 def recherche():
     return render_template('recherche.html')
 
-@app.route('/resultats', methods=['GET', 'POST'])
+@application.route('/resultats', methods=['GET', 'POST'])
 def resultats():
     if request.method == 'POST':
         results = addok2map.lookup(request.form['address'])
@@ -39,8 +39,8 @@ def resultats():
 
 
 def main():
-    app.debug = False
-    app.run()
+    application.debug = False
+    application.run()
 
 
 if __name__ == '__main__':
